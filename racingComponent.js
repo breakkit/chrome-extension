1c3d708969fe214b400b20529f174856d2097037
breakkit <kalaharidesert123@gmail.com>
2016-02-10 06:25:59 +0800

    commit sixth

--- racingComponent.js
+++ racingComponent.js
@@ -326,7 +326,7 @@ function RacingWPTable(rNo) {
     this.clone = function(obj) {
         if (obj == null || typeof(obj) != 'object') return obj;
         var temp = new obj.constructor();
-        for (var key in obj) temp[key] = clone(obj[key]);
+        for (var key in obj) temp[key] = this.clone(obj[key]);
         return temp;
     }
     this.generateInnerTable = function() {
