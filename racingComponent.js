c84358fc16fd4b844411dc2b4e7b5ef60eee8238
breakkit <kalaharidesert123@gmail.com>
2016-02-10 04:33:48 +0800

    this is a very fdskfljsioqfjdsklsfjdsklsldk commit

--- racingComponent.js
+++ racingComponent.js
@@ -527,10 +527,11 @@ function RacingWPTable(rNo) {
                     else buf.append('<span class="wpTdColor" style="color:').append(getOddsFgColor(this.winColorInd[i])).append(';background-color:').append(getOddsBgColor(this.winColorInd[i])).append('">').append(this.winOdds[i]).append('&nbsp</span>');
                     buf.append('</td>');
                     //test
-                    this.winOdds.sort(function(a, b) {
+                    var sortedWinOdds = this.winOdds;
+                    sortedWinOdds.sort(function(a, b) {
                         return a - b;
                     });
-                    console.log(this.winOdds);
+                    console.log(sortedWinOdds);
                     //fdsfs
                     //test
                     // place odds
