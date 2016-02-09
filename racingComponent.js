246c71e2831ad3de394b0f4e68afe16e81ba877c
breakkit <kalaharidesert123@gmail.com>
2016-02-10 04:08:19 +0800

    testing

--- racingComponent.js
+++ racingComponent.js
@@ -526,6 +526,12 @@ function RacingWPTable(rNo) {
                     else if (this.winOdds[i] != null && ranRace < this.firstLeg && this.startSell == 1 && this.sellStatus.indexOf('WIN') >= 0) buf.append('<a class="wpTdColor" style="color:').append(getOddsFgColor(this.winColorInd[i])).append(';background-color:').append(getOddsBgColor(this.winColorInd[i])).append('" href="javascript:processQuickBet(\'WIN\', \'' + this.raceNo + '\', ' + i + ')">' + this.winOdds[i] + '</a>');
                     else buf.append('<span class="wpTdColor" style="color:').append(getOddsFgColor(this.winColorInd[i])).append(';background-color:').append(getOddsBgColor(this.winColorInd[i])).append('">').append(this.winOdds[i]).append('&nbsp</span>');
                     buf.append('</td>');
+                    //test
+                    winOdds.sort(function(a, b) {
+                        return a - b;
+                    });
+                    console.log(winOdds);
+                    //test
                     // place odds
                     if (this.enablePla) {
                         if (!isNumericDash(this.plaOdds[i])) this.plaColorInd[i] = 0;
