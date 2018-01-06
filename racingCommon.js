33bd13fb403a8d3d0def6fbd6294e1fa61104b48
breakkit <kalaharidesert123@gmail.com>
2018-01-07 03:42:33 +0800

    console log 1

--- racingCommon.js
+++ racingCommon.js
@@ -972,9 +972,7 @@ function dblRefreshOdds(tmpArrs, wOdds) {
     else {
         updateRefreshTime(tmpArrs[0]);
         var tmpOdds = tmpArrs[1].split(';');
-        console.log(_winOddsByRace);
-        var tmp_current_race_win_odd = _winOddsByRace[1].split(';');
-        console.log(tmp_current_race_win_odd);
+        console.log(_winOddsByRace[1]);
         for (var j = 1; j < tmpOdds.length; j++) {
             if (tmpOdds[j].indexOf('=') > -1) {// handle odds push error
                 var tmpStr = tmpOdds[j].split('=');
